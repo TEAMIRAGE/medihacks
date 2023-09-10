a2e321f8ebdd9c98fdda9121b0ca06e9de10ab65
Giridhari Singh Bisht <grsb2002@gmail.com>
2023-09-10 11:48:43 +0530

    Update router.js

--- server/router.js
+++ server/router.js
@@ -43,3 +43,114 @@ const storage = new Storage({
   keyFilename: gcpJsonKey, 
 });
 const bucket = storage.bucket(gcpBucketName);
+
+
+
+
+
+
+
+//*****************************************************Page Render*************************************************
+route.get('/',(req,res)=>{
+    res.render('home.ejs');
+});
+
+
+route.get('/bloodReport-Analysis',requireAuth,(req,res)=>{
+  res.render('manual.ejs');
+  });
+
+
+route.get('/aboutUs', (req,res)=>{
+  res.render('aboutUs.ejs');
+})
+
+
+
+
+// IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII COLLECTION PAGE IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+
+
+
+
+route.get('/user-collection',requireAuth, async (req,res)=>{
+
+
+  try {
+   
+    const jwtToken = req.cookies.jwt;
+    let userId;
+if (jwtToken) {
+  
+    const decodedToken = jwt.verify(jwtToken, secretJwtKey);
+    
+    
+    console.log('Decoded Token inside API: '+decodedToken);
+    
+  
+     userId = decodedToken.id;
+     console.log("user ID ki values: " + userId);
+}
+
+ const fractureImageNames = await fracture_Database.find({ UserId:userId });
+
+    
+    const fractureSignedURLs = await Promise.all(
+      fractureImageNames.map(async (image) => {
+        const [signedURL] = await bucket.file(image.Imagename).getSignedUrl({
+            action: 'read',
+            expires: Date.now() + 60 * 60 * 1000, // 1 hour
+          });
+
+        return {signedURL, date: image.date, output: image.output, uniqueID : image._id};
+      })
+    );
+
+// -----------------------------
+
+    const bloodReportFileNames = await bloodReport_Database.find({ UserId:userId });
+    
+    
+    
+    const bloodReportSignedURLs = await Promise.all(
+      bloodReportFileNames.map(async (image) => {
+        const [signedURL] = await bucket.file(image.Imagename).getSignedUrl({
+            action: 'read',
+            expires: Date.now() + 60 * 60 * 1000, // 1 hour
+          });
+         
+        return {signedURL, date: image.date, uniqueID : image._id};
+      })
+    );
+
+// ------------------------------------
+
+
+
+const tumorFileNames = await tumor_Database.find({ UserId:userId });
+
+    
+const tumorSignedURLs = await Promise.all(
+  tumorFileNames.map(async (image) => {
+    const [signedURL] = await bucket.file(image.Imagename).getSignedUrl({
+        action: 'read',
+        expires: Date.now() + 60 * 60 * 1000, // 1 hour
+      });
+     
+    return {signedURL, date: image.date, output: image.output, uniqueID : image._id};
+  })
+);
+
+
+
+
+
+res.render('collections.ejs', { fractureSignedURLs , bloodReportSignedURLs, tumorSignedURLs});
+
+  } catch (error) {
+    console.error('Error retrieving images:', error);
+    res.status(500).send('Internal Server Error');
+  }
+
+
+}) ;
